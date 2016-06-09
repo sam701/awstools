71e80eab8540c32494b47f6593eaca81450ee6d8
Alexei Samokvalov <a.sam701@gmail.com>
2016-06-09 16:40:04 +0200

    fixed iteration over shards

--- kinesis.go
+++ kinesis.go
@@ -40,17 +40,7 @@ func kinesisPrintRecords(c *cli.Context) error {
 	}
 
 	for _, shard := range dsr.StreamDescription.Shards {
-
-		itOut, err := client.GetShardIterator(&kinesis.GetShardIteratorInput{
-			StreamName:        aws.String(streamName),
-			ShardId:           shard.ShardId,
-			ShardIteratorType: aws.String("LATEST"),
-		})
-		if err != nil {
-			log.Fatalln("ERROR", err)
-		}
-
-		go searchInShard(client, itOut.ShardIterator, c.StringSlice("pattern"))
+		go searchInShard(client, streamName, shard.ShardId, c.StringSlice("pattern"))
 	}
 
 	ch := make(chan bool)
@@ -59,39 +49,63 @@ func kinesisPrintRecords(c *cli.Context) error {
 	return nil
 }
 
-func searchInShard(client *kinesis.Kinesis, shardIterator *string, patterns []string) {
+func searchInShard(client *kinesis.Kinesis, streamName string, shardId *string, patterns []string) {
+	itOut, err := client.GetShardIterator(&kinesis.GetShardIteratorInput{
+		StreamName:        aws.String(streamName),
+		ShardId:           shardId,
+		ShardIteratorType: aws.String("LATEST"),
+	})
+	if err != nil {
+		log.Fatalln("ERROR", err)
+	}
+
+	shardIterator := itOut.ShardIterator
+
 	for {
+		if shardIterator == nil {
+			return
+		}
 		rOut, err := client.GetRecords(&kinesis.GetRecordsInput{
 			ShardIterator: shardIterator,
 		})
 		if err != nil {
 			log.Fatalln("ERROR", err)
 		}
+		shardIterator = rOut.NextShardIterator
 
 		for _, record := range rOut.Records {
 			if len(patterns) == 0 {
-				printKinesisEvent(record, "")
+				printKinesisEvent(record, patterns)
 			} else {
 				str := string(record.Data)
+				matched := true
 				for _, pat := range patterns {
-					if strings.Contains(str, pat) {
-						printKinesisEvent(record, pat)
+					if !strings.Contains(str, pat) {
+						matched = false
+						break
 					}
+				}
+				if matched {
+					printKinesisEvent(record, patterns)
 				}
 			}
 		}
+
+		time.Sleep(1 * time.Second)
 	}
 }
 
-func printKinesisEvent(record *kinesis.Record, pattern string) {
+func printKinesisEvent(record *kinesis.Record, patterns []string) {
 	tt := record.ApproximateArrivalTimestamp.Format(time.RFC3339)
 	tt = rgbterm.FgString(tt, 100, 255, 100)
 	fmt.Print(tt + " ")
 	str := strings.TrimSpace(string(record.Data))
-	if pattern == "" {
+	if len(patterns) == 0 {
 		fmt.Println(str)
 	} else {
-		str = strings.Replace(str, pattern, rgbterm.FgString(pattern, 255, 100, 100), -1)
+		for _, pat := range patterns {
+			str = strings.Replace(str, pat, rgbterm.FgString(pat, 255, 100, 100), -1)
+		}
 		fmt.Println(str)
 	}
 }
