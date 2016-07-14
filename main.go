c1c17a25e5c472960c2f0465ed7948d10bbdee7a
Alexei Samokvalov <a.sam701@gmail.com>
2016-07-14 14:58:35 +0200

    version 0.8.0

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "awstools"
-	app.Version = "0.7.0"
+	app.Version = "0.8.0"
 	app.Usage = "AWS tools"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
